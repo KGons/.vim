910b78d6002714c9a9c02d8cb9f3a7940daa0682
Kyrre Gonsholt <kyrre.gonsholt@defa.com>
2015-11-04 09:06:18 +0100

    install.sh is deprecated, using install.py instead

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim "+source .vimrc" +PluginInstall +qall
-cd ~/.vim/bundle/YouCompleteMe && ./install.sh
+d ~/.vim/bundle/YouCompleteMe && ./install.py
